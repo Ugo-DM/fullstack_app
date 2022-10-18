9cdc4c2afc6beacb2fb137550a2cfd9fb766cd1c
Ugo-OClock <ugodemarchdelmissier@gmail.com>
2022-10-18 15:02:08 +0200

    gestion url

--- public/index.php
+++ public/index.php
@@ -1,6 +1,21 @@
 <?php
 require '../vendor/autoload.php';
 
+if(isset($_GET['page']) && $_GET['page'] === '1') {
+    
+    $uri = explode('?', $_SERVER['REQUEST_URI'])[0];
+    $get = $_GET;
+    unset($get['page']);
+    $query = http_build_query($get);
+    if(!empty($query)) {
+       $uri = $uri . '?' . $query;
+    }
+    
+    http_response_code(301);
+    header('Location ' . $uri);
+    exit();
+}
+
 $router = new App\Router(dirname(__DIR__) . '/views');
 $router
     ->get('/', 'post/index', 'home')
--- views/post/index.php
+++ views/post/index.php
@@ -1,12 +1,22 @@
 <?php
 use App\Helpers\Text;
 use App\Model\Post;
+use App\Connection;
+use App\URL;
 
 $title = 'Mon  Blog';
-$pdo = new PDO('mysql:dbname=tutoblog;host=127.0.0.1', 'root', 'Gb77//@dP10', [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-]);
-$query = $pdo->query('SELECT * FROM post ORDER BY created_at DESC LIMIT 12');
+$pdo = Connection::getPDO();
+
+$currentPage = URL::getPositiveInt('page', 1);
+
+$count = (int)$pdo->query('SELECT COUNT(id) FROM post')->fetch(PDO::FETCH_NUM)[0];
+$perPage = 12;
+$pages = ceil($count / $perPage);
+if($currentPage == $pages) {
+    throw new Exception('Cette page n\'existe pas');
+}
+$offset = $perPage * ($currentPage - 1);
+$query = $pdo->query("SELECT * FROM post ORDER BY created_at DESC LIMIT $perPage OFFSET $offset");
 $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
 
 ?>
@@ -19,4 +29,18 @@ $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
         <?php require 'card.php' ?>
     </div>
     <?php endforeach ?>
+</div>
+
+
+<div class="d-flex justify-content-between my-4">
+    <?php if($currentPage > 1): ?>
+    <?php
+    $link = $router->url('home');
+    if($currentPage > 2) $link .= '?page=' . ($currentPage -1);
+    ?>
+    <a href="<?= $link ?>" class="btn btn-primary">&laquo; Page précédente</a>
+    <?php endif ?>
+    <?php if($currentPage < $pages): ?>
+    <a href="<?= $router->url('home')?>?page=<?=$currentPage + 1?>"class="btn btn-primary ml-auto">&raquo; Page suivante</a>
+    <?php endif ?>
 </div>
