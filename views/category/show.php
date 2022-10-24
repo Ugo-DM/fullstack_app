f61ce6d72f2a21f41fc134c418cd340a05e437b8
Ugo-OClock <ugodemarchdelmissier@gmail.com>
2022-10-24 12:55:36 +0200

    refacto-pagination

--- views/category/show.php
+++ views/category/show.php
@@ -1,8 +1,8 @@
 <?php
 use App\Connection;
 use App\Model\Category;
-use App\URL;
 use App\Model\Post;
+use App\PaginatedQuery;
 
 $id = (int)$params['id'];
 $slug = $params['slug'];
@@ -25,28 +25,18 @@ if ($category->getSlug() !== $slug) {
 
 $title = "Catégorie {$category->getName()}";
 
-
-$currentPage = URL::getPositiveInt('page', 1);
-
-$count = (int)$pdo->query('SELECT COUNT(category_id) FROM post_category WHERE category_id = ' . $category->getID())->fetch(PDO::FETCH_NUM)[0];
-$perPage = 12;
-$pages = ceil($count / $perPage);
-if($currentPage == $pages) {
-    throw new Exception('Cette page n\'existe pas');
-}
-$offset = $perPage * ($currentPage - 1);
-$query = $pdo->query("
+$paginatedQuery = new PaginatedQuery("
     SELECT p.* 
     FROM post p
     JOIN post_category pc ON pc.post_id = p.id
     WHERE pc.category_id = {$category->getID()}
-    ORDER BY created_at 
-    DESC LIMIT $perPage OFFSET $offset
-");
-$posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
+    ORDER BY created_at ",
+    "SELECT COUNT(category_id) FROM post_category WHERE category_id = {$category->getID()}"
+   
+);
+/** @var Post[]*/
+$posts = $paginatedQuery->getItems( Post::class);
 $link= $router->url('category', ['id' => $category->getID(), 'slug' => $category->getSlug()])
-
-
 ?>
 
 <h1><?= htmlentities($title) ?></h1>
@@ -61,14 +51,6 @@ $link= $router->url('category', ['id' => $category->getID(), 'slug' => $category
 
 
 <div class="d-flex justify-content-between my-4">
-    <?php if($currentPage > 1): ?>
-    <?php
-    $l = $link;
-    if($currentPage > 2) $l .= $link . '?page=' . ($currentPage -1);
-    ?>
-    <a href="<?= $l ?>" class="btn btn-primary">&laquo; Page précédente</a>
-    <?php endif ?>
-    <?php if($currentPage < $pages): ?>
-    <a href="<?= $link ?>?page=<?=$currentPage + 1?>"class="btn btn-primary ml-auto">&raquo; Page suivante</a>
-    <?php endif ?>
+    <?= $paginatedQuery->previousLink($link) ?>
+    <?= $paginatedQuery->nextLink($link) ?>
 </div>
--- views/post/index.php
+++ views/post/index.php
@@ -2,22 +2,19 @@
 use App\Helpers\Text;
 use App\Model\Post;
 use App\Connection;
+use App\PaginatedQuery;
 use App\URL;
 
 $title = 'Mon  Blog';
 $pdo = Connection::getPDO();
 
-$currentPage = URL::getPositiveInt('page', 1);
+$paginatedQuery = new PaginatedQuery(
+    "SELECT * FROM post ORDER BY created_at DESC",
+    "SELECT COUNT(id) FROM post"
+);
 
-$count = (int)$pdo->query('SELECT COUNT(id) FROM post')->fetch(PDO::FETCH_NUM)[0];
-$perPage = 12;
-$pages = ceil($count / $perPage);
-if($currentPage == $pages) {
-    throw new Exception('Cette page n\'existe pas');
-}
-$offset = $perPage * ($currentPage - 1);
-$query = $pdo->query("SELECT * FROM post ORDER BY created_at DESC LIMIT $perPage OFFSET $offset");
-$posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
+$posts = $paginatedQuery->getItems(Post::class);
+$link = $router->url('home');
 
 ?>
 
@@ -33,14 +30,6 @@ $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
 
 
 <div class="d-flex justify-content-between my-4">
-    <?php if($currentPage > 1): ?>
-    <?php
-    $link = $router->url('home');
-    if($currentPage > 2) $link .= '?page=' . ($currentPage -1);
-    ?>
-    <a href="<?= $link ?>" class="btn btn-primary">&laquo; Page précédente</a>
-    <?php endif ?>
-    <?php if($currentPage < $pages): ?>
-    <a href="<?= $router->url('home')?>?page=<?=$currentPage + 1?>"class="btn btn-primary ml-auto">&raquo; Page suivante</a>
-    <?php endif ?>
+    <?= $paginatedQuery->previousLink($link);?>
+    <?= $paginatedQuery->nextLink($link);?>
 </div>
